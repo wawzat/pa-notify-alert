88415346f94a1bab53498020e08a2e7e5519781b
wawzat <wawzat@gmail.com>
2023-10-04 11:15:29 -0700

    Update map links

--- constants.py
+++ constants.py
@@ -26,7 +26,7 @@ OPEN_AQI_ALERT_THRESHOLD = 140
 
 SUBJECT = 'pa.notify.alert - PurpleAir Sensor Air Quality Alert'
 EMAIL_BODY_INTRO = 'High AQI Notification From pa.notify.alert'
-PA_MAP_EMAIL_LINK = '<a href="https://map.purpleair.com/1/mPM25/a10/p604800/cC0#11.64/33.7686/-117.4475">PurpleAir Map</a>'
+PA_MAP_EMAIL_LINK = '<a href="https://map.purpleair.com/1/i/mAQI/a0/p604800/cC5?select=70745#14.28/38.01731/-122.55343">PurpleAir Map</a>'
 EMAIL_DISCLAIMER_PT1 = 'The information provided in this message is for notification purposes only. ' \
             'Prior to making any decisions, please independently verify the information ' \
             'is accurate through official sources.'
@@ -37,4 +37,4 @@ EMAIL_DISCLAIMER_PT3 = 'AQI provided in this notification is based on PM 2.5 par
             'ground-level ozone, carbon monoxide, sulfur dioxide, and nitrogen dioxide ' \
             'may also be present but are not included in this notification.'
 
-PA_MAP_TEXT_LINK = 'https://map.purpleair.com/1/i/mAQI/a0/p604800/cC0?select=70745#14.28/38.01731/-122.55343'
+PA_MAP_TEXT_LINK = 'https://map.purpleair.com/1/i/mAQI/a0/p604800/cC5?select=70745#14.28/38.01731/-122.55343'
