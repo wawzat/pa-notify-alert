9d710dd6bf654e9689a1d172031d5e31b4096cf7
wawzat <wawzat@gmail.com>
2023-10-08 08:50:52 -0700

    pa_get_regional_data() temp add lat lon

--- pa_notify_alert.py
+++ pa_notify_alert.py
@@ -255,7 +255,7 @@ def get_local_pa_data(sensor_id) -> tuple:
     root_url: str = 'https://api.purpleair.com/v1/sensors/{sensor_id}?fields={fields}'
     params = {
         'sensor_id': sensor_id,
-        'fields': "name,humidity,pm2.5_atm_a,pm2.5_atm_b,pm2.5_cf_1_a,pm2.5_cf_1_b"
+        'fields': "name,humidity,pm2.5_atm_a,pm2.5_atm_b,pm2.5_cf_1_a,pm2.5_cf_1_b, latitude, longitude"
     }
     url: str = root_url.format(**params)
     try:
