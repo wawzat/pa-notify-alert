c0cdbb30f885afb2fefe93a2f8def7b1afdecfe8
wawzat <wawzat@gmail.com>
2023-10-21 12:06:04 -0700

    RC1.3

--- pa_notify_alert.py
+++ pa_notify_alert.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # Regularly polls Purpleair api for outdoor sensor data and sends notifications via text or email when air quality exceeds threshold.
-# James S. Lucas - 20231013
+# James S. Lucas - 20231021
 
 import os
 import sys
