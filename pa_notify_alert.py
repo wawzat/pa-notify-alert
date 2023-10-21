1fe24f62024b43595af8ec92d1abfefcc36a74c8
wawzat <wawzat@gmail.com>
2023-10-21 11:52:08 -0700

    text_notify() Formatting

--- pa_notify_alert.py
+++ pa_notify_alert.py
@@ -466,7 +466,7 @@ def text_notify(is_daily: bool,
                 f' AQI: {local_pm25_aqi} \n'
                 f' {rate_of_change_text} \n'
                 f' {local_pm25_aqi_avg_duration:.0f} Min. Avg AQI: {local_pm25_aqi_avg:.0f} \n' 
-                f' {confidence_text} '
+                f' {confidence_text} \n'
                 f'Neighborhood \n'
                 f'\u00A0\u00A0Avg AQI: {regional_aqi_mean:.0f} \n \n'
                 f'{constants.PA_MAP_TEXT_LINK} '
