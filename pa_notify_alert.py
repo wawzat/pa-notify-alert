5cc875de899ed2a73cd5e6933f1cf1f0d9a8efd8
wawzat <wawzat@gmail.com>
2023-11-14 06:12:38 -0800

    email_notify() time formatting

--- pa_notify_alert.py
+++ pa_notify_alert.py
@@ -544,7 +544,7 @@ def email_notify(
     elif round(pm_aqi_roc, 1) > 0:
         rate_of_change_text = f'Air quality has increased by {abs(pm_aqi_roc):.1f} AQI per minute over the last {local_pm25_aqi_avg_duration:.0f} minutes'
     else:
-        rate_of_change_text = f'Air quality has not changed in the last {local_pm25_aqi_avg_duration} minutes'
+        rate_of_change_text = f'Air quality has not changed in the last {local_pm25_aqi_avg_duration:0f} minutes'
     if confidence == 'LOW':
         confidence_text = 'Sensor accuracy is low, the sensor may need cleaning. Please obtain accurate data through official sources. <br>'
     else:
